15449f5ed0f2264694e74762fc843ce2dcb95dde
Kiran9901 <pskiran9901@gmail.com>
2020-11-23 10:47:43 +0530

    main:Updated code

--- angular-ui/src/app/student/student.component.ts
+++ angular-ui/src/app/student/student.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent implements OnInit {
+  subjects: String[];
+  selectedtopic: String;
+  selectedsubtopic: String;
+  subtopics: String[];
+  
+   
 
   constructor() { }
-
+  
   ngOnInit(): void {
+    this.subjects = ["OS", "DAA", "FLAT"];
+   
   }
+
+  onSubjectSelected(val: any)
+  {
+    console.log("Hello");
+    if(val == "OS")
+    {
+        this.subtopics = ["CPU Scheduling","Memory Management","I/O Management"];
+    }
+    else if(val == "DAA")
+    {
+        this.subtopics = ["Branch and Bound","Travelling Salesman","Dynamic Programming"];
+    }
+    else if (val == "FLAT")
+    {
+        this.subtopics = ["Pushdown Automata","State Diagram","Turing Machine"];
+    }
+  }
+    
 
 }
